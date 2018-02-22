216bbcf1dec6bb87ab950d9e6e543d484c82326e
Abel <ledezmatto@tecnodesign.com.py>
2018-02-22 11:24:07 -0300

    Postgres Database

--- invasion/settings.py
+++ invasion/settings.py
@@ -77,13 +77,26 @@ WSGI_APPLICATION = 'invasion.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+
+DATABASES = {
+    'default': {
+     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     'NAME': 'invasion',
+     'USER': 'invasion',
+     'PASSWORD': 'tecnodesign!PY',
+     'HOST': 'localhost',
+     'PORT': 5432,
+    }
+}
+
 
 
 # Password validation
