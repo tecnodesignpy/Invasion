0fe14680f19731a012eea7dc71d1b38386f7e532
Abel <ledezmatto@tecnodesign.com.py>
2018-02-22 12:00:02 -0300

    url /inscripcion

--- invasion/urls.py
+++ invasion/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^inscripcion/', include('inscripciones.urls', namespace="inscripcion")),
     url(r'^$', include('inscripciones.urls', namespace="inscripcion")),
 ]
