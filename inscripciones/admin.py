7f84ab6b533cdff7780dff9ce2927e106a84549b
Abel <ledezmatto@tecnodesign.com.py>
2018-02-23 20:43:31 -0300

    Buscador en el Admin

--- inscripciones/admin.py
+++ inscripciones/admin.py
@@ -5,55 +5,11 @@ from . import models
 from django.utils.html import format_html
 from django.core.urlresolvers import reverse
 
-class EdadFilter(admin.SimpleListFilter):
-    # Human-readable title which will be displayed in the
-    # right admin sidebar just above the filter options.
-    title = ('Age')
-
-    # Parameter for the filter that will be used in the URL query.
-    parameter_name = 'edad'
-
-    def lookups(self, request, model_admin):
-        """
-        Returns a list of tuples. The first element in each
-        tuple is the coded value for the option that will
-        appear in the URL query. The second element is the
-        human-readable name for the option that will appear
-        in the right sidebar.
-        """
-        return (
-            ('1', '18-25'),
-            ('2', '26-35'),
-            ('3', '36-40'),
-            ('4', '41+'),
-        )
-
-    def queryset(self, request, queryset):
-        """
-        Returns the filtered queryset based on the value
-        provided in the query string and retrievable via
-        `self.value()`.
-        """
-        # Compare the requested value (either '80s' or '90s')
-        # to decide how to filter the queryset.
-        if self.value() == '1':
-            return queryset.filter(edad__gte=18,
-                                    edad__lte=25)
-        if self.value() == '2':
-            return queryset.filter(edad__gte=26,
-                                    edad__lte=35)
-        if self.value() == '3':
-            return queryset.filter(edad__gte=36,
-                                    edad__lte=40)
-        if self.value() == '4':
-            return queryset.filter(edad__gte=41,
-                                    edad__lte=99)
-
 	
 class FormularioAdmin(admin.ModelAdmin):
     list_display = (u'id','fecha_completado','nombres','apellidos','edad','nacimiento','lider_celula','pagado','fecha_pagado','account_actions',)
     #list_filter = (EdadFilter,NacionalidadFilter,'genero','nivel_ingles','experiencia_years','fumador','licencia_usa','recontratado','trabajo',)
-    #search_fields = ['pais','nombres','edad','id','apellidos','experiencia','company_name','Experiencia_Laboral','comment','hear_about','recrutier','conocimiento_mecanica','consulado_cercano','ciudad','calle_nombre','email','status','genero','facebook_name','skype_name',]
+    search_fields = ['id','lider_celula','nombres','cedula','apellidos']
     ordering = ('-fecha_completado',)
     save_on_top = True
     readonly_fields = ['fecha_pagado']
